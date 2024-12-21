76c785a3de969e39ba9c0143b9c446dd852d3868
Hnkotnhka <hnkotnhka@gmail.com>
2024-12-21 14:56:49 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+
+        String str = scan.nextLine();
+        System.out.println(calc(str));
+    }
+
+    public static int calc(String input) throws IOException {
+        String[] mas = input.split(" ");
+
+        if (mas.length != 3) throw new IOException();
+
+        int num1 = isNumCorrect(mas[0]);
+        int num2 = isNumCorrect(mas[2]);
+
+        char operator = mas[1].charAt(0);
+
+        return _calc(num1, num2, operator);
+    }
+
+    static int isNumCorrect(String str) throws IOException {
+        int num = Integer.parseInt(str);
+        if (num <= 10 && num > 0) {
+            return num;
+        }
+        throw new IOException();
+    }
+
+    static int _calc(int x, int y, char op) throws IOException {
+
+        switch (op) {
+            case '+' -> {
+                return x + y;
+            }
+            case '-' -> {
+                return x - y;
+            }
+            case '/' -> {
+                return x / y;
+            }
+            case '*' -> {
+                return x * y;
+            }
+        }
+        throw new IOException();
+    }
+}
